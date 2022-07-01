4fa08f090a36a1e42c1e0fd17406b05335e9133b
rjjfajardo <rjjfajardo@gmail.com>
2022-07-01 13:40:42 +0800

    Category-CRUD

--- client/src/index.js
+++ client/src/index.js
@@ -6,21 +6,25 @@ import App from "../src/pages/App";
 
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { swrConfig } from "./utils/swr";
+import { SWRConfig } from "swr";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <SnackbarProvider
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      maxSnack={3}
-      preventDuplicate
-      variant="info"
-      autoHideDuration={2000}
-    >
-      <App />
-    </SnackbarProvider>
+    <SWRConfig value={swrConfig}>
+      <SnackbarProvider
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        maxSnack={3}
+        preventDuplicate
+        variant="info"
+        autoHideDuration={2000}
+      >
+        <App />
+      </SnackbarProvider>
+    </SWRConfig>
   </Provider>
 );
