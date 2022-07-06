7b78aec0bae51aa24c2d1418ba76cd5d21ffa822
rjjfajardo <rjjfajardo@gmail.com>
2022-07-06 18:59:33 +0800

    rebasing

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { SnackbarProvider } from "notistack";
 
+<<<<<<< HEAD
 import App from '../src/pages/App'
+=======
+import App from "./App";
+>>>>>>> 3b95f2d ([FEATURE][FE] Admin Sign Up)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
