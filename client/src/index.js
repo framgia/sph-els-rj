517423222e809bcfe2e6632b1ef715b6f1f5c3b4
rjjfajardo <rjjfajardo@gmail.com>
2022-07-08 09:54:57 +0800

    rebasing(1)

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { SnackbarProvider } from "notistack";
 
 import App from "../src/pages/App";
+<<<<<<< HEAD
 
 import { store } from "./store";
 import { Provider } from "react-redux";
+=======
+>>>>>>> 390ba4a (rebasing(1))
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
